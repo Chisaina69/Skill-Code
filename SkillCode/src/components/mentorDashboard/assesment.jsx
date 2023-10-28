7eb95f4b124e92a3b6e0e3e93741e95f8f174f99
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-10-28 12:46:39 +0300

    Mentor navbar

---
+++ SkillCode/src/components/mentorDashboard/assesment.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import '../App.css';
+
+
+function assesment(){
+ return(
+    <div className = 'mentor'>
+    <h1 className='logo'> SkillCode</h1>
+    <ul className='navbar'>
+      <li>Profile</li>
+      <li>Assesment</li>
+      <li>Grades</li>
+      <li>Home</li>
+      <li>Help</li>
+      <li>Logout</li>
+
+    </ul>
+    </div>
+    
+ )
+
+}
+
+export default assesment;

382e5e702904f7bb07301fa25920165da5bdf89c
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-10-30 15:39:29 +0300

    Revert "Christine"

--- SkillCode/src/App.jsx
+++ SkillCode/src/App.jsx
@@ -2,23 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Student from './components/studentDashboard/studentdashboard';
 import AssessmentComponent from './components/studentDashboard/Assesment';
-import Login from './components/Authentication/Login';
-import Register from './components/Authentication/Register';
-import HomePage from './components/HomePage';
-import './App.css'; 
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Student />} />
         <Route path="/assessment" element={<AssessmentComponent />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
   );
 }
 
 export default App;
-
 

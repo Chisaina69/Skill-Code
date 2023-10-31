4980465f9f0a66f8667a2dfe866c13ffe8e8fb87
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-10-31 12:24:21 +0300

    changed some profile data

--- SkillCode/src/App.jsx
+++ SkillCode/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 import Student from './components/studentDashboard/studentdashboard';
 import AssessmentComponent from './components/studentDashboard/Assesment';
 import StudentProfile from './components/studentDashboard/profile';

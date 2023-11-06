5e4ade12a83ecf78efb0722ab492de48ec67c3d9
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-11-06 11:13:56 +0300

    navbar styling

--- SkillCode/src/components/mentorDashboard/Navbar.jsx
+++ SkillCode/src/components/mentorDashboard/Navbar.jsx
@@ -5,27 +5,27 @@ import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
 const NavBar = () => (
     <div className="font-sans text-base w-1/4 p-10">
-      <h1 className='"font-medium text-lg p-4"'>SkillCode</h1>
+      <h1 className='font-semibold text-blue-950 text-lg p-4'>SkillCode</h1>
       <div className='p-4'>
         <div >
-          <ul>
-            <li className='mb-4 flex items-center'>
-              <Link to='/profile'>Profile</Link>
+          <ul>                 
+            <li className='mb-4  flex items-center'>
+              <Link className='text-zinc-950 font-semibold'to='/profile'>Profile</Link>
             </li>
             <li className='mb-4 flex items-center'>
-              <Link to='/assessment'>Assessment</Link>
+              <Link  className='text-zinc-950 font-semibold' to='/assessment'>Assessment</Link>
             </li>
             <li className='mb-4 flex items-center'>
-              <Link to='/grades'>Grades</Link>
+              <Link className='text-zinc-950 font-semibold'to='/grades'>Grades</Link>
             </li>
             <li className='mb-4 flex items-center' >
-              <Link to='/home'>Home</Link>
+              <Link className='text-zinc-950 font-semibold' to='/home'>Home</Link>
             </li>
             <li className='mb-4 flex items-center'>
-              <Link to='/help'>Help</Link>
+              <Link className='text-zinc-950 font-semibold'to='/help'>Help</Link>
             </li>
             <li className='mb-4 flex items-center'>
-              <Link to='/logout'>Logout</Link>
+              <Link className='text-zinc-950 font-semibold' to='/logout'>Logout</Link>
             </li>
           </ul>
         </div>
